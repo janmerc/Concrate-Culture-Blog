ffa8af195059b61b18bcadba1305851405ee34dd
Merc Jan <honza.merc@gmail.com>
2020-03-13 14:15:50 +0100

    prihlaseni php

---
+++ prihlaseni.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+require "config.php";
+
+$username = $_POST["name"];
+$password = $_POST["password"];
+
+$spojeni = new PDO("mysql:dbname=" . DATABAZENAME . ";host=" . SERVER, USERNAME, PASSWORD);
+
+$dotaz = $spojeni->prepare("SELECT * FROM user WHERE `name` = ?");
+$dotaz->execute([$username]);
+
+$user = $dotaz->fetch();
+
+if ($user === false) {
+    echo("Špatné jméno");
+    exit;
+}
+
+if (!password_verify($password , $user["password"])) {
+    echo("Špatné heslo");
+    exit;
+}
+
+$_SESSION["userId"] = $user["id"];
