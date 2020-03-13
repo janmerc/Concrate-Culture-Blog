3ce746a4b286cd1c1137453ad11e30cfe05c7309
Merc Jan <honza.merc@gmail.com>
2020-03-13 13:31:53 +0100

    změna configu
    naprosté předělání podle nového návodu, zatím bez reálných udajů

--- config.php
+++ config.php
@@ -1,33 +1,5 @@
 <?php
-ob_start();
-session_start();
-
-define('DBHOST','localhost');
-define('DBUSER','database username');
-define('DBPASS','database password');
-define('DBNAME','database name');
-
-$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-date_default_timezone_set('Europe/Prague');
-
-function __autoload($class) {
-   
-    $class = strtolower($class);
- 
-    $classpath = 'classes/class.'.$class . '.php';
-    if ( file_exists($classpath)) {
-       require_once $classpath;
-     }     
- 
-    $classpath = '../classes/class.'.$class . '.php';
-    if ( file_exists($classpath)) {
-       require_once $classpath;
-     }
-         
-      
- }
- 
- $user = new User($db); 
+const SERVER="localhost";
+const DATABAZENAME="blog";
+const USERNAME="root";
+const PASSWORD=""; 
