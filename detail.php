3f02c6c5e28ecec0f5af09e1647ebdb29f5e0d43
Merc Jan <honza.merc@gmail.com>
2020-03-14 03:34:13 +0100

    uprava html

--- detail.php
+++ detail.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css" /> 
     <title>Concrate Culture</title>
 </head>
 <body>
+<div id="wrapper">
+        <section id="navbar">
+            <h1><a href="index.php">Concrate Culture</a></h1>
+            <hr />
+            <ul>
+                <li><a href="#BETON">BETON</a></li>
+                <li><a href="#SUBWAY RIDERS">SUBWAY RIDERS</a></li>
+                <li><a href="#TELECOM">TELECOM</a></li>
+                <li><a href="#blbos">blbos</a></li>
+              </ul>
+        </section>
 
 
 <?php
@@ -20,9 +32,10 @@ $post = $dotaz->fetch();
 
 $title = $post["title"];
 $content = $post["content"];
-
+echo('<div id="post">');
 echo("<h1>$title</h1>");
 echo("<p>$content</p>");
+echo("</div>");
 ?>
 
 
